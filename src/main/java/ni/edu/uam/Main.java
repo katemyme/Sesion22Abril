320bcbca9cd314857a4b9e3e003637e4d5106da8
ValcocerLopez <vmalcocer@uamv.edu.ni>
2026-04-22 16:35:01 -0600

    Agregando nuevo menu

--- src/main/java/ni/edu/uam/Main.java
+++ src/main/java/ni/edu/uam/Main.java
@@ -1,19 +1,54 @@
 package ni.edu.uam;
 
-import ni.edu.uam.modelos.Producto;
 import ni.edu.uam.servicios.ProductoServicio;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String[] args) {
         ProductoServicio productos = new ProductoServicio();
-        productos.agregarProducto("Tajadas con quesp", 60, 3);
-        productos.agregarProducto("Cafe",40,3);
 
-        System.out.println( " Factura ");
-        System.out.println(productos.getProductos());
-        System.out.println("Total a pagar " + productos.getMonto());
+        int opcion;
+
+        do {
+            opcion = Integer.parseInt(JOptionPane.showInputDialog(
+                    "MENU \n" +
+                            "1. Agregar producto\n" +
+                            "2. Mostrar productos\n" +
+                            "3. Salir\n" +
+                            "Elige una opción:"
+            ));
+
+            switch (opcion) {
+                case 1:
+                    String nombre = JOptionPane.showInputDialog("Nombre del producto:");
+
+                    double precio = Double.parseDouble(JOptionPane.showInputDialog("Precio:")
+                    );
+
+                    int cantidad = Integer.parseInt(JOptionPane.showInputDialog("Cantidad:")
+                    );
+                    productos.agregarProducto(nombre, precio, cantidad);
+
+                    JOptionPane.showMessageDialog(null, "Producto agregado correctamente.");
+                    break;
+
+                case 2:
+                    String mensaje = "-FACTURA -\n";
+                    mensaje += productos.getProductos() + "\n";
+                    mensaje += "Total a pagar: " + productos.getMonto();
+
+                    JOptionPane.showMessageDialog(null, mensaje);
+                    break;
+
+                case 3:
+                    JOptionPane.showMessageDialog(null, "Saliendo...");
+                    break;
+
+                default:
+                    JOptionPane.showMessageDialog(null, "Opción inválida");
+            }
+
+        } while (opcion != 3);
     }
 }
